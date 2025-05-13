48d6e91aa1733dbac4edc61df728005c972ff847
Faycal Ghozzi <faycal.ghozzi@gmail.com>
2025-05-13 11:39:38 +0100

    add callback function

--- server/main.py
+++ server/main.py
@@ -12,6 +12,20 @@ app = FastAPI()
 
 @app.get("/login")
 def login():
-    url = f"https://accounts.spotify.com/authorize?client_id={SPOTIFY_CLIENT_ID}&response_type=code&redirect_uri={REDIRECT_URI}&scope={SCOPES}"
-    return RedirectResponse(url)
+    return RedirectResponse(
+        f"https://accounts.spotify.com/authorize?client_id={SPOTIFY_CLIENT_ID}&response_type=code"
+        f"&redirect_uri={REDIRECT_URI}&scope=user-library-read playlist-read-private"
+    )
 
+@app.get("/callback")
+def callback(code: str):
+    data = {
+        "grant_type": "authorization_code",
+        "code": code,
+        "redirect_uri": REDIRECT_URI,
+        "client_id": SPOTIFY_CLIENT_ID,
+        "client_secret": SPOTIFY_CLIENT_SECRET
+    }
+    headers = {"Content-Type": "application/x-www-form-urlencoded"}
+    r = requests.post("https://accounts.spotify.com/api/token", data=data, headers=headers)
+    return r.json()
